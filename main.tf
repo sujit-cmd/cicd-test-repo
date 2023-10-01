3cb78adeb4edc579a1c71f5b0beaecfa5019cf2a
sujit-student <sujit341@gmail.com>
2023-10-01 22:50:16 +0530

    commit to dev

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "static-bucket" {
-  name          = "github-cicd-demo"
+  name          = "github-cicd-demo-1"
   project = "my-tftest-project"
   location      = "EU"
   force_destroy = true
