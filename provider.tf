e280c649cd4d34d2842217dfddec11d85a9f2ed0
sujit-student <sujit341@gmail.com>
2023-10-01 23:30:19 +0530

    state-file-updated

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.83.0"
+    }
+  }
+}
+
+provider "google" {
+    project     = "my-tftest-project"
+    region      = "us-central1"
+   
+}
